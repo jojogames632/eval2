5de2bfb08b506dc4138871bca6b90a9c67d37660
jojo <jojogames@free.fr>
2021-04-09 13:00:24 +0200

    Add node_modules, script.js and images folder

---
+++ script.js
@@ -0,0 +1,21 @@
+// Jonathan Viault 
+
+// Buttons
+let newGameBtn = document.getElementById('newGameBtn');
+let rollBtn = document.getElementById('rollBtn');
+let holdBtn = document.getElementById('holdBtn');
+// Points
+let snakePoints = document.getElementById('snake-points');
+let spiderPoints = document.getElementById('spider-points');
+let currentPoints = document.getElementById('current-points');
+
+const rollDice = () => {
+
+}
+
+const hold = () => {
+
+}
+
+rollBtn.addEventListener('click', rollDice);
+holdBtn.addEventListener('click', hold);
